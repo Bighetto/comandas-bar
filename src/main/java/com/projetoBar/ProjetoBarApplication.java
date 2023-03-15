c56674b8212cba4e6453807333cd1023ae0fc168
Arthur Bighetto <arthurbighetto36@gmail.com>
2023-03-15 13:08:51 -0300

    ajustado requisicao liberando cors na configuracao de seguranca da API e adicionado Documemntacao no Swagger

--- src/main/java/com/projetoBar/ProjetoBarApplication.java
+++ src/main/java/com/projetoBar/ProjetoBarApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EnableSpringDataWebSupport
 @EnableCaching
 @EnableJpaRepositories
+@EnableSwagger2
 public class ProjetoBarApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/projetoBar/configuration/SecurityConfig.java
+++ src/main/java/com/projetoBar/configuration/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.projetoBar.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -17,9 +18,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth)
             throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("admin").password("{noop}admin").roles("ADMIN", "USER")
-                .and()
-                .withUser("user").password("{noop}user").roles("USER");
+                .withUser("arthur").password("{noop}teste").roles("ADMIN");
+//                .and()
+//                .withUser("user").password("{noop}user").roles("USER");
     }
 
     @Override
@@ -27,14 +28,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .httpBasic()
                 .and()
-                .authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/produtos").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET, "/produtos").permitAll()
-                .antMatchers(HttpMethod.PUT, "/produtos").hasRole("ADMIN")
-                .antMatchers(HttpMethod.DELETE, "/produtos").hasRole("ADMIN")
-                .anyRequest().authenticated()
+                .cors()
                 .and()
-                .csrf().disable()
-                .formLogin().disable();
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/swagger-ui.html").permitAll()
+                .anyRequest().authenticated();
+//                .antMatchers("/produtos", "/produtos/**").hasRole("ADMIN");
+//                .antMatchers(HttpMethod.GET, "/produtos").permitAll()
+//                .antMatchers(HttpMethod.PUT, "/produtos").hasRole("ADMIN")
+//                .antMatchers(HttpMethod.DELETE, "/produtos").hasRole("ADMIN")
+
     }
+
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//                .inMemoryAuthentication()
+//                .withUser("arthur").password("{noop}password").roles("ADMIN");
+//    }
 }
